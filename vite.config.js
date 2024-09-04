1766c9f6a1dc25f6afbb53b7c349f5709fc5caa3
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-04 12:34:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/free-brands-svg-icons']
-    }
-  }
+  plugins: [react()]
 });
