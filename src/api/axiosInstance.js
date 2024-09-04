687dcacf3dc8b1c55db26ca8c9ab35f57eb22902
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-04 11:45:22 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://airbnb-clone-backend-1-7s14.onrender.com', // Adjust as per your backend URL
+  baseURL: 'https://airbnb-clone-backend-1-7s14.onrender.com/', // Backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // This allows cookies to be sent and received (useful for authentication)
+  timeout: 30000, // Set a timeout for requests
 });
 
 export default axiosInstance;
