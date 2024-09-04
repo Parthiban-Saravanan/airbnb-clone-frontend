3a1fd3b146ae77ab25a8d0437a3e7adddfa21ab6
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-04 12:36:33 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <Link className="navbar-brand" to="/">
-          <FontAwesomeIcon icon={faAirbnb} flip style={{color: "#ad0101",}} className="me-2"/> {/* Icon added here */}
+          <FontAwesomeIcon icon={faAirbnb} flip style={{color: "#ad0101"}} className="me-2"/> {/* Icon added here */}
           Airbnb Clone
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
