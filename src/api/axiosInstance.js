016fde2a90b4dbd3a41e1c7a7b7cebaef49c0b66
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-09-09 01:57:27 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -6,7 +6,6 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
   withCredentials: true, // This allows cookies to be sent and received (useful for authentication)
-  timeout: 30000, // Set a timeout for requests
 });
 
 export default axiosInstance;
