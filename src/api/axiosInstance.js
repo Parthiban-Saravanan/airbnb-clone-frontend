074a7e22550905ef35c1cba08c36781745af8cb4
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-10-14 08:47:26 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'https://airbnb-clone-backend-1-7s14.onrender.com',
-});
-
-instance.interceptors.push({
-  request: (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+const axiosInstance = axios.create({
+  baseURL: 'https://airbnb-clone-backend-1-7s14.onrender.com', // Backend base URL
+  headers: {
+    'Content-Type': 'application/json',
   },
+  withCredentials: true, // This allows cookies to be sent and received (useful for authentication)
 });
 
-export default instance;
+export default axiosInstance;
